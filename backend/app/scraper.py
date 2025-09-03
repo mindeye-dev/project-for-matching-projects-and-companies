1d7af64e9c3e7f17a2914a8e333f7c9d681f44d9
cg98020455 <charlesgagnon055@gmail.com>
2025-09-04 03:32:31 +0900

    feat: make synchromized multiple scraping

--- backend/app/scraper.py
+++ backend/app/scraper.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
+
 def scrape_all_sources():
     # TODO: Implement scraping logic for all banks/donors
-    print('Scraping all sources...')
- 
+    print("Scraping all sources...")
+
+
 scheduler = BackgroundScheduler()
-scheduler.add_job(scrape_all_sources, 'interval', days=1)
-scheduler.start() 
+scheduler.add_job(scrape_all_sources, "interval", days=1)
+scheduler.start()
